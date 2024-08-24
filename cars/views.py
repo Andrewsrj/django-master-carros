69abf9a4930bed8fe8401e30938e78821785976d
Andrews Costa <andrews.mp@live.com>
2024-08-24 14:06:13 -0300

    feat(style): Lista de carros dinâmica

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Car
 
 # Create your views here.
 
 def cars_view(request):
-    return render(request, 'cars.html')
+    carros = Car.objects.all().order_by('model')
+    context = {
+        'cars': carros,
+    }
+    return render(request, 'cars.html', context)
