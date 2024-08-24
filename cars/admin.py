677bedc9e2f053c9816701b70a6dfae4c0618dd1
Andrews Costa <andrews.mp@live.com>
2024-08-24 13:10:27 -0300

    feat(core): Busca por marca

--- cars/admin.py
+++ cars/admin.py
@@ -5,6 +5,6 @@ from .models import Car
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
-    search_fields = ('model',)
+    search_fields = ('model','brand',)
 
 admin.site.register(Car, CarAdmin)
