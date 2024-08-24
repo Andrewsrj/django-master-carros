e7184e77de23277efa72a43b2738cf0bf65beaf8
Andrews Costa <andrews.mp@live.com>
2024-08-24 13:26:48 -0300

    fix(core): Ajuste de modelo Marca

--- cars/admin.py
+++ cars/admin.py
@@ -5,11 +5,11 @@ from .models import Car, Brand
 
 class BrandAdmin(admin.ModelAdmin):
     list_display = ('name',)
-    search_field = ('name',)
+    search_fields = ('name',)
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
     search_fields = ('model','brand',)
 
-admin.site.register(Car, CarAdmin)
 admin.site.register(Brand, BrandAdmin)
+admin.site.register(Car, CarAdmin)
