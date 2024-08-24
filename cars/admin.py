42a27603c43cebfb354186c74927dfa0fe14e6b0
Andrews Costa <andrews.mp@live.com>
2024-08-24 13:22:03 -0300

    fix(core): Ajuste de modelo Car

--- cars/admin.py
+++ cars/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Car
+from .models import Car, Brand
 
 # Register your models here.
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_field = ('name',)
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
     search_fields = ('model','brand',)
 
 admin.site.register(Car, CarAdmin)
+admin.site.register(Brand, BrandAdmin)
