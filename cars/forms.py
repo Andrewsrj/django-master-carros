9079549af9902196348d4cb81990bc3cbfd666ad
Andrews Costa <andrews.mp@live.com>
2024-10-20 20:07:41 -0300

    feat(core): Validação de dados

--- cars/forms.py
+++ cars/forms.py
@@ -5,3 +5,10 @@ class CarModelForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
+
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 1:
+            self.add_error('value', 'O valor mínimo deve ser de R$1')
+        return value
+    
